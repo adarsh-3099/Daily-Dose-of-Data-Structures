cff9d0609f2700399cdbc4240ed1e2deca824ef2
Adarsh Kumar Singh <adarshsinghrathore3.com>
2022-11-02 19:41:01 +0530

    soem changes

---
+++ Binary Trees/Binary Tree Preorder Traversal.py
@@ -0,0 +1,31 @@
+"""Recursive"""
+class Solution:
+   def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+      
+       def T(r):
+           if not r:
+               return
+           self.li.append(r.val)
+           T(r.left)
+           T(r.right)
+          
+       self.li = []
+       T(root)
+       return self.li
+	
+"""Iterative
+	→ Take a stack and store node of tree.
+	→ Pop the top element and print it then store right child and left child."""
+class Solution:
+   def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+      
+       def T(r):
+           if not r:
+               return
+           self.li.append(r.val)
+           T(r.left)
+           T(r.right)
+          
+       self.li = []
+       T(root)
+       return self.li
